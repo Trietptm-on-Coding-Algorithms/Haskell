ff455fa2d0c0c7eb94ef9461d5ea0e681a597632
nero-luci <cornel.punga@gmail.com>
2015-02-15 13:04:49 +0200

    [Haskell|Miscellaneous] Added Chop8 utility function

---
+++ Miscellaneous/Chop8.hs
@@ -0,0 +1,9 @@
+-- Chops a list into lists of at most eight bits
+import Unfold
+type Bit = Integer
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 [] = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
+
+chop8' = unfold null (take 8) (drop 8)
